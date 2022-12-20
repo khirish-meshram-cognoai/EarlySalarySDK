09e4cf66900c4be69acdccbf4d4ec9763a6fa0e0
Khirish7 <khirish1311@gmail.com>
2022-12-20 21:59:36 +0530

    Finished EarlySalarySDK

---
+++ EarlySalarySDK/EarlySalarySDK/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  EarlySalarySDK
+//
+//  Created by Khirish Meshram on 20/12/22.
+//
+
+import Foundation
+
+public class Constants {
+    
+    public static var botUrl = ""
+    public static var botId = ""
+    public static var accessToken = ""
+    public static var customerId = ""
+    public static var isTokenVerify = false
+    public static let tokenVerificationUrl = "/chat/verify-access-token/"
+    
+}
---
+++ EarlySalarySDK/EarlySalarySDK/EarlySalarySDK.h
@@ -0,0 +1,18 @@
+//
+//  EarlySalarySDK.h
+//  EarlySalarySDK
+//
+//  Created by Khirish Meshram on 20/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EarlySalarySDK.
+FOUNDATION_EXPORT double EarlySalarySDKVersionNumber;
+
+//! Project version string for EarlySalarySDK.
+FOUNDATION_EXPORT const unsigned char EarlySalarySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EarlySalarySDK/PublicHeader.h>
+
+
